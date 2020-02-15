3c3b16a8682b140eff96a169ef7f582fe3972f88
PointGuard <point_guard@hotmail.com>
2020-02-15 16:54:55 -0600

    comment added

--- src/main/java/com/stack/restservices/springbootbuildingblocks/DemoApplication.java
+++ src/main/java/com/stack/restservices/springbootbuildingblocks/DemoApplication.java
@@ -3,6 +3,7 @@ package com.stack.restservices.springbootbuildingblocks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// comment here
 @SpringBootApplication
 public class DemoApplication {
 
