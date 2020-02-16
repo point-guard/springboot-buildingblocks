d86d39193d98be3c70c3e4ac9350f96817df8fcc
PointGuard <point_guard@hotmail.com>
2020-02-16 14:20:32 -0600

    JPA Base RestServices Development Completed

---
+++ src/main/java/com/stack/restservices/entities/User.java
@@ -0,0 +1,108 @@
+package com.stack.restservices.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "USER_NAME", length = 50, nullable = false, unique = true)
+    private String userName;
+
+    @Column(name = "FIRST_NAME", length = 50, nullable = false)
+    private String firstName;
+
+    @Column(name = "LAST_NAME", length = 50, nullable = false)
+    private String lastName;
+
+    @Column(name = "EMAIL_ADDRESS", length = 50, nullable = false)
+    private String email;
+
+    @Column(name = "ROLE", length = 50, nullable = false)
+    private String role;
+
+    @Column(name = "SSN", length = 50, nullable = false, unique = true)
+    private String ssn;
+
+    public User() {
+    }
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public User(Long id, String userName, String firstName, String lastName, String email, String role, String ssn) {
+        this.id = id;
+        this.ssn = ssn;
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSsn() {
+        return this.ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'batman@home.net', 'Batman', 'Lakers', 'admin', 'ssn101', 'batman')
+insert into user values(102, 'arrow@home.net', 'Arrow', 'Spurs', 'admin', 'ssn102', 'arrow')
+insert into user values(103, 'thor@home.net', 'Thor', 'Thunder', 'admin', 'ssn103', 'thor')
